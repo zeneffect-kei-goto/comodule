10a32b43e01f9ab7a3cf30c13068c47bf925ff05
Kei Goto <kgoto@zeneffect.co.jp>
2014-03-30 13:21:40 +0900

    CustomizeClass loads custom classes automatically by autoload

--- lib/comodule.rb
+++ lib/comodule.rb
@@ -5,4 +5,9 @@ module Comodule
   autoload :UniArray,       'comodule/uni_array'
   autoload :ConfigSupport,  'comodule/config_support'
   autoload :CustomizeClass, 'comodule/customize_class'
+
+  module CustomizeClass
+    autoload :HashCustom,   'comodule/customize_class/hash_custom'
+    autoload :StringCustom, 'comodule/customize_class/string_custom'
+  end
 end
