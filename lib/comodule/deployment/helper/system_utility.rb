fa78228e4bb6456da62e7fffb47c991ff87c9789
Kei Goto <kgoto@zeneffect.co.jp>
2014-08-03 21:45:02 +0900

    delete Deployment::Helper::SystemUtility#render_in_tmp

--- lib/comodule/deployment/helper/system_utility.rb
+++ lib/comodule/deployment/helper/system_utility.rb
@@ -1,57 +1,48 @@
 module Comodule::Deployment::Helper::SystemUtility
 
-  def self.included(receiver)
-    receiver.send :include, InstanceMethods
+  def render(path)
+    ERB.new(File.read(path)).result(binding)
   end
 
-  module InstanceMethods
-
-    def render(path)
-      ERB.new(File.read(path)).result(binding)
+  def render_in_path(from, to)
+    File.open(to, 'w') do |file|
+      file.write render(from)
     end
 
-    def render_in_dir(file_path, in_dir)
-      dir, filename = File.split(file_path)
+    to
+  end
 
-      path_in_dir = File.join(in_dir, filename.sub(/\.erb$/, ''))
+  def render_in_dir(file_path, in_dir)
+    dir, filename = File.split(file_path)
 
-      File.open(path_in_dir, 'w') do |file|
-        file.write render(file_path)
-      end
+    path_in_dir = File.join(in_dir, filename.sub(/\.erb$/, ''))
 
-      path_in_dir
+    render_in_path file_path, path_in_dir
+  end
+
+  def yaml_to_config(path)
+    ::Comodule::ConfigSupport::Config.new(
+      YAML.load_file( path ) || {}
+    )
+  end
+
+  def be_dir(*dir)
+    if dir.size > 1
+      return dir.map { |d| be_dir(d) }
     end
 
-    def render_in_tmp(file_path, tmp_dir)
-      return unless file_path =~ /\.erb$/
+    dir = dir[0]
+    FileUtils.mkdir_p dir unless File.directory?(dir)
+    dir
+  end
 
-      render_in_dir file_path, tmp_dir
+  def be_file(*path)
+    if path.size > 1
+      return path.map { |p| be_file(p) }
     end
 
-    def yaml_to_config(path)
-      ::Comodule::ConfigSupport::Config.new(
-        YAML.load( File.read(path) )
-      )
-    end
-
-    def be_dir(*dir)
-      if dir.size > 1
-        return dir.map { |d| be_dir(d) }
-      end
-
-      dir = dir[0]
-      FileUtils.mkdir_p dir unless File.directory?(dir)
-      dir
-    end
-
-    def be_file(*path)
-      if path.size > 1
-        return path.map { |p| be_file(p) }
-      end
-
-      path = path[0]
-      FileUtils.touch path if !File.directory?(path) && !File.file?(path)
-      path
-    end
+    path = path[0]
+    FileUtils.touch path if !File.directory?(path) && !File.file?(path)
+    path
   end
 end
